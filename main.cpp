01542af3deeb67d8482f7513091cebe82a2de308
darleg <darell.legare@outlook.com>
2021-04-10 14:35:50 -0700

    add random rgb and leds

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mbed.h"
 #include "console.h"
-#include "OnBoard.h"
+#include "oneRGB3LEDs.h"
 #include "DevI2C.h"
 #include "HTS221Sensor.h"
 #include "LPS22HBSensor.h"
 
-
+// Initialise the digital pins for LEDs as an output
+    DigitalOut ledg(led1);  // WiFi or same as LED1
+    DigitalOut ledb(led2);  // Azure
+    DigitalOut ledy(led3);  // User
+    
+    // Initialise the pulse-width-moudulation for RGB LED
+    PwmOut RGBR(RedRGB);
+    PwmOut RGBG(GreenRGB);
+    PwmOut RGBB(BlueRGB);
 
 #define delay200ms     200ms
 #define delay500ms     500ms
@@ -24,7 +32,7 @@ static HTS221Sensor HumTemp(&devI2c);
 
 int main()
 {
-    uint8_t id;
+    uint8_t id1, id2;
     float temp1, hum, temp2, press;
 
     /* Init all sensors with default params */
@@ -35,10 +43,10 @@ int main()
     HumTemp.enable();
     PressTemp.enable();
     
-    HumTemp.read_id(&id);
-    printf("HTS221  humidity & temperature    = 0x%X\r\n", id);
-    PressTemp.read_id(&id);
-    printf("LPS22HB pressure & temperature    = 0x%X\r\n", id);
+    HumTemp.read_id(&id1);
+    printf("HTS221  humidity & temperature    = 0x%X\r\n", id1);
+    PressTemp.read_id(&id2);
+    printf("LPS22HB pressure & temperature    = 0x%X\r\n", id2);
     
     printf("\n\r--- Reading sensor values ---\n\r"); ;
     
@@ -59,6 +67,18 @@ int main()
          printf("Temp: %f.2 C\r\n", temp2);
          printf("Press: %.2f mbar\r\n", press);
          printf("---\r\n");
+
+         RGBR = 0;
+        RGBG = rand() % 100;
+        ledg = !ledg;
+        ThisThread::sleep_for(delay200ms);
+        RGBG = 0;
+        RGBB = rand() % 100;
+        ledb = !ledb;
+        ThisThread::sleep_for(delay200ms);
+        RGBB = 0;
+        RGBR = rand() % 100;
+        ledy = !ledy;
          
          ThisThread::sleep_for(delay800ms);
 
